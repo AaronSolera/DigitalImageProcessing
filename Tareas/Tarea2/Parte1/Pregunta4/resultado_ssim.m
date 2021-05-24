1d9568743dc659e0bff3bb224d42d01d6ef42180
AaronSolera <aaron.smartinez1997@gmail.com>
2021-05-23 23:24:16 -0600

    Tarea 2 commit final

--- Tareas/Tarea2/Parte1/Pregunta4/resultado_ssim.m
+++ Tareas/Tarea2/Parte1/Pregunta4/resultado_ssim.m
@@ -8,22 +8,31 @@ V_0 = VideoReader('../Pregunta2/files/video.mp4');
 V_1 = VideoReader('../Pregunta3/files/video_sin_ruido_alg1.mp4');
 V_2 = VideoReader('../Pregunta3/files/video_sin_ruido_alg2.mp4');
 % Número de Frames (Imágenes)
-fr = V_0.NumberOfFrames; 
+fr = V_1.NumberOfFrames
 
-H_V1 = [];
-H_V2 = [];
+H_V1 = zeros(fr);
+H_V2 = zeros(fr);
+V_x  = zeros(fr);
+
 
 for k = 1 : fr
   F_V  = readFrame(V_0);
   F_V1 = readFrame(V_1);
   F_V2 = readFrame(V_2);
-  %
-  H_V1(end+1) = ssim(F_V(:,:,1), F_V1(:,:,1));
-  H_V2(end+1) = ssim(F_V(:,:,1), F_V2(:,:,1));
+  
+  [H_V1(k),extra] = ssim(F_V(:,:,1), F_V1(:,:,1));
+  [H_V2(k),extra] = ssim(F_V(:,:,1), F_V2(:,:,1));
+  V_x(k) = k; 
 end
 
-hist(H_V1);
-hist(H_V2);
+
+subplot(1,2,1);
+plot(H_V1);
+title("FMFA")
+
+subplot(1,2,2);
+plot(H_V2);
+title("IAMFA-I")
 
 close(V_1)
 close(V_2)
